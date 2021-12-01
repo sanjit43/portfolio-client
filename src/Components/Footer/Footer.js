d1bd34895ce946bfe4a92650a6a02fc9639b2137
sanjit43 <sanjitsarker257@gmail.com>
2021-12-01 15:17:03 +0600

    footer animation delete

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,14 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+
 
 const Footer = () => {
-    useEffect(() => {
-        AOS.init({ duration: 2000 })
-    }, [])
+
     return (
-        <div data-AOS="fade-up" className='container bg-dark mt-5 text-center'>
+        <div className='container bg-dark mt-5 text-center'>
             <h2 className='text-white'>Follow Me</h2>
             <a href='https://github.com/sanjit43/'><i className="fab fa-github text-light"></i></a>
             <a className='m-3' href='https://www.linkedin.com/in/sanjit-sarker-66848996/'><i className="fab fa-linkedin text-light"></i></a>
